8354b8b56e8b95bc1344a201c609f1e43f03de74
pankajpatel <patel.pankaj@live.in>
2023-01-04 10:34:36 +0000

    Deploying to gh-pages from  @ 5aaad0c35531338821a39ae44fe1fe3c21400b4e 🚀

--- precache-manifest.8a71a38a4858d575edac34f3ceed104e.js
+++ precache-manifest.8a71a38a4858d575edac34f3ceed104e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb2e4343c805cb64c041f7d12daa89b3",
+    "revision": "6ecb8330913d5604f946184034b22761",
     "url": "/todo-hooks-context/index.html"
   },
   {
-    "revision": "812a014df5198b561749",
+    "revision": "733702f3ca0cc662ea90",
     "url": "/todo-hooks-context/static/css/2.80f479d8.chunk.css"
   },
   {
-    "revision": "812a014df5198b561749",
-    "url": "/todo-hooks-context/static/js/2.ee35cddb.chunk.js"
+    "revision": "733702f3ca0cc662ea90",
+    "url": "/todo-hooks-context/static/js/2.1d0f9be7.chunk.js"
   },
   {
     "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
-    "url": "/todo-hooks-context/static/js/2.ee35cddb.chunk.js.LICENSE"
+    "url": "/todo-hooks-context/static/js/2.1d0f9be7.chunk.js.LICENSE"
   },
   {
-    "revision": "9f9fea3e7f87ddf7669e",
-    "url": "/todo-hooks-context/static/js/main.e2ca382c.chunk.js"
+    "revision": "8825b3ef18b2614be26d",
+    "url": "/todo-hooks-context/static/js/main.ef095e7d.chunk.js"
   },
   {
     "revision": "a32f2b422690e98c1840",
